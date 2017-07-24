6241021512d6324904357cdc0bb9bab11295f802
Jay C <j3chowdh@edu.uwaterloo.ca>
2017-07-24 10:45:31 -0400

    Update assignment.js

--- Western/assignment.js
+++ Western/assignment.js
@@ -1,14 +1,11 @@
-var title;
-var due_date;
-var grade;
-var comments;
+class Assignment {
 
-
-function Assignment(title, due_date, grade, comments) {
-    this.title = title;
-    this.due_date = due_date;
-    this.grade = grade;
-    this.comments = comments;
+    constructor(title, due_date, grade, comments) {
+        this.title = title;
+        this.due_date = due_date;
+        this.grade = grade;
+        this.comments = comments;
+    }
 }
 
 // Export class Assignment.
