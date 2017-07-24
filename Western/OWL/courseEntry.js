d1904fade5bab1662b59a4f6618563477e88c076
Jay C <j3chowdh@edu.uwaterloo.ca>
2017-07-24 15:35:01 -0400

    Update courseEntry.js

--- Western/OWL/courseEntry.js
+++ Western/OWL/courseEntry.js
@@ -1,46 +1,42 @@
-var osmosis = require('osmosis');
-var async = require("async");
+const osmosis = require('osmosis');
+const course = require('../course');
 
-var Course = require('../course');
-
-// Western OWL cookie headers...
-var COOKIE_SESSION_ID = 'JSESSIONID';
-var COOKIE_ENCRYPYION = 'NSC_pxm.vxp.db--443';
-
-var courses = new Array();
+// Western OWL cookie headers.
+const COOKIE_SESSION_ID = 'JSESSIONID';
+const COOKIE_ENCRYPYION = 'NSC_pxm.vxp.db--443';
 
 function courseEntry(url, cookies) {
+    let courses = [];
     return new Promise((resolve, reject) => {
         osmosis
         .get(url)
         .cookie(COOKIE_SESSION_ID, cookies[COOKIE_SESSION_ID])
         .cookie(COOKIE_ENCRYPYION, cookies[COOKIE_ENCRYPYION])
-        // Find all occurances of table row (each course has its own row).
         .find('tr')
-        // Parse information from inside the table row.
         .set({
             'course': 'td[headers="type"]',
             'link': 'h4 > a@href',
             'name': 'h4 > a[href]',
             'creator': 'td[headers="createdBy"]'
         })
+        // Follow all occurances of the first <h4> <a/> </h4> within each <tr>.
         .follow('td[headers="title"] > h4 a[1]')
+        // Each course sets one or the other title value to <a>.
         .follow('a[title="For storing and computing assessment grades from Tests & Quizzes or that are manually entered"] @href, a[title="Gradebook"] @href')
+        // Get the direct iframe url..
         .set({
             'grade_url': 'div.title @href'
         })
         .data(function(data) {
+            console.log(data.name);
+            // Table may contain outliers.
             if (data.course === 'course') {
                 // Name contains title and section.
-                console.log(data.name);
                 var name = data.name.split(' ');
-                courses.push(new Course(name[0] + ' ' + name[1], data.creator, name[2], data.link, data.grade_url));
+                courses.push(new Course((name[0] + ' ' + name[1]), data.creator, name[2], data.link, data.grade_url));
             }
         })
-        .done(function() {
-            // Return courses object.
-            resolve(courses);
-        })
+        .done(() => resolve(courses))
     })
 }
 
